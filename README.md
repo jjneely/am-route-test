505295d7597485092c307e91f580824058eeb377
Jack Neely <jjneely@42lines.net>
2018-04-06 15:53:54 -0400

    LICENSE, README, some testing enhancements

---
+++ README.md
@@ -0,0 +1,49 @@
+Alertmanager Route Test
+=======================
+
+I use Alertmanager[1] with Prometheus to route alerts from Prometheus servers
+to the right team.  This also does various filtering, handle high and low
+priority alerts, and other things which have made the routing tree more complex
+than I'd like.  Often, requests to fine tune various bits come in that require
+some modification of the route tree.  Because alert notification simply cannot
+break, I wanted a tool that could automate unit testing of my route tree.
+
+This lead to writing of `am-route-test`.  This will parse a given Alertmanager
+config, use the label / value pairs supplied on the command line, and print
+out the Alertmanager receivers that would eventually handle that an alert
+with those labels.
+
+```
+Usage of am-route-test:
+Evalute what receiver(s) are used when an alert with the given labels
+is passed through the routing tree found in the Alertmanager config.
+
+        am-route-test [options] label=value [label=value ...]
+
+  -e string
+        Comma seperated list of expected alert receivers
+  -f string
+        Alertmanager configuration file (default "alertmanager.yml")
+
+```
+
+Use the `-e` option to make the tool exit with a status code of 2 if the
+receivers found do not match the comma seperated list of receivers passed to
+that flag.
+
+Maintenance
+-----------
+
+Alertmanager is not meant to be used as a library, so its completely vendored
+into this source code.  Maintenance and upgrades should include updating
+the vendoring.  Perhaps even managing it with `govendor` or `dep`.
+
+Could this be contributed directly to the `amtool` CLI that ships with
+Alertmanager?
+
+Contributing
+------------
+
+Open a PR.
+
+[1]: https://github.com/prometheus/alertmanager
--- main.go
+++ main.go
@@ -12,6 +12,8 @@ import (
 	"gopkg.in/yaml.v2"
 	"io/ioutil"
 	"log"
+	"os"
+	"sort"
 	"strings"
 )
 
@@ -23,11 +25,31 @@ import (
 	"github.com/prometheus/common/model"
 )
 
+func usage() {
+	fmt.Fprintf(flag.CommandLine.Output(), "Usage of %s:\n", os.Args[0])
+	fmt.Fprintf(flag.CommandLine.Output(),
+		"Evalute what receiver(s) are used when an alert with the given labels\n")
+	fmt.Fprintf(flag.CommandLine.Output(),
+		"is passed through the routing tree found in the Alertmanager config.\n\n")
+	fmt.Fprintf(flag.CommandLine.Output(),
+		"\t%s [options] label=value [label=value ...]\n\n", os.Args[0])
+	flag.PrintDefaults()
+
+	os.Exit(1)
+}
+
 func main() {
 	var ctree config.Config
-	configFile := flag.String("-f", "alertmanager.yml",
+	flag.Usage = usage
+	configFile := flag.String("f", "alertmanager.yml",
 		"Alertmanager configuration file")
+	expectedReceivers := flag.String("e", "",
+		"Comma seperated list of expected alert receivers")
 	flag.Parse()
+
+	if flag.NArg() == 0 {
+		usage()
+	}
 
 	// Read the configuration file
 	blob, err := ioutil.ReadFile(*configFile)
@@ -57,7 +79,29 @@ func main() {
 
 	// Okay, what does the routing say?
 	routes := routeTree.Match(lset)
+	results := []string{}
 	for _, route := range routes {
-		fmt.Printf("%s\n", route.RouteOpts.Receiver)
+		results = append(results, route.RouteOpts.Receiver)
+	}
+	sort.Strings(results)
+	for _, receiver := range results {
+		log.Printf("Executed receiver: %s", receiver)
+	}
+
+	// Compare to expected results
+	if *expectedReceivers == "" {
+		os.Exit(0)
+	}
+	expected := strings.Split(*expectedReceivers, ",")
+	sort.Strings(expected)
+	if len(expected) != len(results) {
+		log.Printf("FAILED: number of receivers does not match number of expected")
+		os.Exit(2)
+	}
+	for i := range expected {
+		if expected[i] != results[i] {
+			log.Printf("FAILED: Receiver(s) do not match expected.")
+			os.Exit(2)
+		}
 	}
 }
